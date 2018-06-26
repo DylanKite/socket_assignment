58dde1766c5b182e7674beb1620d589cc78fd5e8
Dylan Kite <dylan44117@gmail.com>
2018-06-25 20:07:02 -0400

    client/src/main.c

--- client/includes/client.h
+++ client/includes/client.h
@@ -1,1 +1,18 @@
+typedef struct {
+    char name[50];
+    int id;
+    int street_num;
+    char street_name[60];
+    char city[25];
+    char postal_code[10];
+    char province[20];
+    char country[20];
+} user_info;
+
 int init_client(char IP_address[], unsigned int port);
+int close_connection();
+
+int send_message(user_info to_send);
+
+int write_user_to_file(user_info to_write);
+int read_user_file(user_info *read_user);
--- client/src/client.c
+++ client/src/client.c
@@ -6,11 +6,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
 #define SUCCESS 1
-
+#define FILEPATH_CSV "user_info.csv"
 static int client_id =0;
 
 int init_client(char IP_address[], unsigned int port)
@@ -36,3 +41,49 @@ int init_client(char IP_address[], unsigned int port)
         return SUCCESS;
 }
 
+int send_message(user_info to_send)
+{
+    send(client_id, &to_send, sizeof(user_info), 0);
+}
+
+int write_user_to_file(user_info to_write)
+{
+    char string_to_write[1000] = {"\0"};
+    int fd = 0;
+    fd = open(FILEPATH_CSV, O_WRONLY | O_CREAT);
+    if(fd < 0) {
+        printf("issue writing to file");
+    }
+    snprintf(string_to_write,sizeof(to_write), "%s, %d, %d, %s, %s, %s, %s, %s",
+            to_write.name,
+            to_write.id,
+            to_write.street_num,
+            to_write.street_name,
+            to_write.city,
+            to_write.postal_code,
+            to_write.province,
+            to_write.country);
+    printf("%s writing to file %s\n",__func__, string_to_write);
+
+    write(fd, string_to_write, strlen(string_to_write));
+    close(fd);
+}
+
+int read_user_file(user_info *read_user)
+{
+    char string_to_read[1000];
+    char split_string[100];
+    int fd =0;
+    fd = open(FILEPATH_CSV, O_RDONLY);
+    read(fd,string_to_read,sizeof(string_to_read));
+    strcpy(read_user->name, strtok(string_to_read, ","));
+    read_user->id = atoi(strtok(NULL, ","));
+    read_user->street_num = atoi(strtok(NULL, ","));
+    strcpy(read_user->street_name, strtok(NULL, ","));
+    strcpy(read_user->city, strtok(NULL, ","));
+    strcpy(read_user->postal_code, strtok(NULL, ","));
+    strcpy(read_user->province, strtok(NULL, ","));
+    strcpy(read_user->country, strtok(NULL, ","));
+    strncpy(read_user->country,read_user->country, strlen(read_user->country) - 1);
+    close(fd);
+}
